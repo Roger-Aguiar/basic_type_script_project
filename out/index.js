34e39fbd57a5984c2e708262cc147ed01cc397fa
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-05-10 14:12:39 -0300

    It creates the initial files

--- out/index.js
+++ out/index.js
@@ -2,10 +2,13 @@
 
 let userInput = readline.createInterface({input: process.stdin, output: process.stdout});
 
-userInput.question("Please, enter an integer number: ", 
+userInput.question("Please, enter an integer number: ",
 function(answer)
 {
     let integerNumber = new SumOfDigits(answer);
     let digitalRootSum = integerNumber.digitalRoot();
     console.log(`The digital root sum is: ${digitalRootSum}`);
 });*/
+var message = "Hello world!";
+console.log(message);
+//# sourceMappingURL=index.js.map
